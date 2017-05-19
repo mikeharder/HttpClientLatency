6abe17478fd201d2eb1e513ac401800843136a1e
Mike Harder <mharder@microsoft.com>
2017-05-19 14:48:24 -0700

    Apps and load test to explore performance of HttpClient with occasional server latency spikes

---
+++ Downstream/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Downstream
+{
+    public class Program
+    {
+        private static int _requests;
+
+        public static void Main(string[] args)
+        {
+            new WebHostBuilder()
+                .UseKestrel()
+                .Configure(app => app.Run(async (context) =>
+                {
+                    // Simulate a service which is occasionally slow
+                    if (Interlocked.Increment(ref _requests) % 2000 == 0)
+                    {
+                        await Task.Delay(TimeSpan.FromSeconds(3));
+                    }
+
+                    await context.Response.WriteAsync("Hello World!");
+                }))
+                .UseUrls("http://*:5000")
+                .Build()
+                .Run();
+        }
+    }
+}
---
+++ Upstream/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Net.Http;
+
+namespace Upstream
+{
+    public class Program
+    {
+        private static readonly HttpClient _httpClient = new HttpClient() { BaseAddress = new Uri("http://localhost:5000") };
+
+        public static void Main(string[] args)
+        {
+            new WebHostBuilder()
+                .UseKestrel()
+                .Configure(app => app.Run(async (context) =>
+                {
+                    await context.Response.WriteAsync(await _httpClient.GetStringAsync("/"));
+                }))
+                .UseUrls("http://*:5001")
+                .Build()
+                .Run();
+        }
+    }
+}
