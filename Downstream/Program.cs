d64eab8fc737f1b3340d552a38c5c99caf731e8b
Mike Harder <mharder@microsoft.com>
2018-09-27 15:14:29 -0700

    Simplify Downstream request handler

--- Downstream/Program.cs
+++ Downstream/Program.cs
@@ -6,16 +6,14 @@ namespace Downstream
 {
     public class Program
     {
-        private static byte[] _response = Encoding.UTF8.GetBytes(new string('a', 1024));
+        private const int _responseLength = 1024;
+        private static readonly byte[] _response = Encoding.UTF8.GetBytes(new string('a', _responseLength));
 
         public static void Main(string[] args)
         {
             new WebHostBuilder()
                 .UseKestrel()
-                .Configure(app => app.Run(async (context) =>
-                {
-                    await context.Response.Body.WriteAsync(_response, 0, _response.Length);
-                }))
+                .Configure(app => app.Run(context => context.Response.Body.WriteAsync(_response, 0, _responseLength)))
                 .UseUrls("http://*:5000")
                 .Build()
                 .Run();
